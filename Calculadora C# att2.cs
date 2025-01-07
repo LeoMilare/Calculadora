219847d4467d07f097a9c51b99910571cf948fea
LeoMilare <163869485+LeoMilare@users.noreply.github.com>
2025-01-07 15:12:14 -0300

    Add files via upload
    Segue a primeira atualização da minha calculadora feita em C#. Agora com as funcionalidades de SENO, COSSENO, TANGENTE e PORCENTAGEM. Como minha calculadora necessita de 2 valores, caso selecionar SENO, COSSENO OU TANGENTE, é preciso colocar no primeiro valor real o que gostaria de ser calculado, já no segundo, precisa-se colocar o valor 0.

---
+++ Calculadora C# att2.cs
@@ -0,0 +1,76 @@
+﻿using System;
+class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("digite o primeiro numero");
+        string entrada1 = Console.ReadLine();
+        double numero1 = double.Parse(entrada1);
+
+        Console.WriteLine("digite o segundo numero");
+        string entrada2 = Console.ReadLine();
+        double numero2 = double.Parse(entrada2);
+
+        Console.WriteLine("escolha a operação (+, -, *, /, %, SEN, COS, TAN):");
+        string operacao = Console.ReadLine();
+
+        double resultado = 0;
+
+        if (operacao == "+")
+        {
+            resultado = numero1 + numero2;
+        }
+        else if (operacao == "-")
+        {
+            resultado = numero1 - numero2;
+        }
+        else if (operacao == "*")
+        {
+            resultado = numero1 * numero2;
+        }
+        else if (operacao == "%")
+        {
+            resultado = (numero1 / 100) * numero2;       
+        }
+        else if (operacao == "SEN")
+        {
+            double graus = numero1;
+            double radianos = graus * (Math.PI / 180);
+            double seno = Math.Sin(radianos);
+            resultado = seno;
+        }
+        else if (operacao == "COS")
+        {
+            double graus = numero1;
+            double radianos = graus * (Math.PI / 180);
+            double cosseno = Math.Cos(radianos);
+            resultado = cosseno;
+        }
+        else if (operacao == "TAN")
+        {
+            double graus = numero1;
+            double radianos = graus * (Math.PI / 180);
+            double tangente = Math.Tan(radianos);
+            resultado = tangente;
+        }
+        else if (operacao == "/")
+        {
+            if (numero2 != 0)
+            {
+                resultado = numero1 / numero2;
+            }
+            else
+            {
+                Console.WriteLine("divisão por 0 não é permitida.");
+                return;
+            }
+        }
+        else
+        {
+            Console.WriteLine("operação inválida.");
+            return;
+        }
+
+        Console.WriteLine("resultado: " + resultado);
+    }
+}
